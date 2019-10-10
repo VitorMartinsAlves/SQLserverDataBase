3e270805b01443bc5c0bac6fe4ef0cc463397a8c
Vitor Martins <54907067+VitorMartinsAlves@users.noreply.github.com>
2019-10-10 13:36:28 -0300

    Update

--- DDLs.sql
+++ DDLs.sql
@@ -15,16 +15,16 @@ create table Aluno (
 create table Materia(
     IdMateria INT IDENTITY PRIMARY KEY NOT NULL,
     Materia VARCHAR(50)
-);
+            );
 
--- criando a tabela Trabalho com chaves estrangeiras
-create table Trabalho(
-    IdTrabalho INT IDENTITY PRIMARY KEY NOT NULL,
-    Nota DECIMAL,
-    -- Chaves estrangeiras
-    IdMateria INT FOREIGN KEY (IdMateria) REFERENCES Materia(IdMateria),
-    IdAluno INT FOREIGN KEY (IdAluno) REFERENCES Aluno(IdAluno)
-);
+            -- criando a tabela Trabalho com chaves estrangeiras
+            create table Trabalho(
+                IdTrabalho INT IDENTITY PRIMARY KEY NOT NULL,
+                Nota DECIMAL,
+                -- Chaves estrangeiras
+                IdMateria INT FOREIGN KEY (IdMateria) REFERENCES Materia(IdMateria),
+                IdAluno INT FOREIGN KEY (IdAluno) REFERENCES Aluno(IdAluno)
+            );
 
 /* Alterando uma tabela */
 alter table Materia
@@ -41,7 +41,7 @@ INSERT INTO Aluno (NomeAluno, Ra, Idade) VALUES ('Larissa', 'R123', 20);
 INSERT INTO Materia (Materia) VALUES ('Banco de dados');
 
 /* Mostra dados especificos de um aluno */
---  puxa pelo id os dados referentes ao aluno
+--  puxa pelo   id os dados referentes ao aluno
 
 select * from aluno where IdAluno = 2;
 
@@ -57,3 +57,20 @@ select * from aluno order by Nome asc;
 -- Ordenar por nome do aluno de forma decrescente
 
 select * from aluno order by Nome desc; 
+
+-- Exibir todos os dados da tabela que você juntou
+-- aluno refere-se a tabela aluno e id aluno se refere ao numero do aluno e trabaho puxa pela tabela idaluno a nota do trabalho
+
+inner join aluno on aluno.IdAluno = trabalho.IdAluno;
+
+-- Juntamos as 3 tabelas usando inner  join 
+-- deixamos somentes os dados que nos interessam
+
+Select
+	aluno.Nome,
+	aluno.Ra,
+	aluno.IDADE,
+	materia.Nota
+from tabalho
+inner join materia on materia.IdMateria = trabalho.IdMateria
+inner join aluno on aluno.IdAluno = trabalho.IdAluno;
--- DMLs.sql
+++ DMLs.sql
@@ -4,3 +4,6 @@ insert into aluno (nome, RA, IDADE) values ('Alef' , 'R123' , 23);
 
 /*inserir dado na tabela afetada */
 insert into materia (Materia) values ('Portuges');
+
+
+insert into trabalho (nota, idaluno , idmateria ) values (1,3,4) (13,123,1), ( )
